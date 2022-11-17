0b4a7b14347312eca5f20be9caaedfe7e55dc35e
shiwoo.park <87423085+custardcream98@users.noreply.github.com>
2022-11-17 12:01:13 +0900

    Chore: eslint 수정

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -23,26 +23,38 @@ module.exports = {
     "react-hooks",
     "@typescript-eslint",
     "prettier",
+    "import",
   ],
   rules: {
     "linebreak-style": 0,
-    "import/prefer-default-export": 0,
     "prettier/prettier": 0,
+
+    // import
     "import/extensions": 0,
-    "no-use-before-define": 0,
-    "import/no-unresolved": 0,
-    "import/no-extraneous-dependencies": 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    "import/prefer-default-export": 0,
+    "import/no-unresolved": "error",
+    "import/no-extraneous-dependencies": 0,
+    //
+
+    "no-use-before-define": ["off"],
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "react/jsx-filename-extension": [
+      "warn",
+      { extensions: [".tsx"] },
+    ],
+
     "no-shadow": 0,
     "react/prop-types": 0,
-    "react/jsx-filename-extension": [
-      2,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ],
     "jsx-a11y/no-noninteractive-element-interactions": 0,
   },
   settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
     "import/resolver": {
-      typescript: {},
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
   },
   "lint-staged": {
